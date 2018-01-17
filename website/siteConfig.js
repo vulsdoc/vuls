f9c7916ced4572d960826b424f94b98adf61115f
kota kanbe <kotakanbe@gmail.com>
2018-01-17 12:10:11 +0900

    Update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,79 +15,6 @@ const users = [
   },
 ];
 
-const supportedOSes = [
-  {
-    caption: 'RedHat Enterprise Linux',
-    image: '/img/redhat-logo_640x480.jpg',
-    infoLink: 'https://www.prettier.io',
-    // fbOpenSource: false,
-    pinned: true,
-  },
-  {
-    caption: 'FastText',
-    image: '/img/fasttext.png',
-    infoLink: 'https://fasttext.cc',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'Jest',
-    image: '/img/jest.png',
-    infoLink: 'https://facebook.github.io/jest/',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'Reason React',
-    image: '/img/reason-react.svg',
-    infoLink: 'https://reasonml.github.io/reason-react/',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'MakeItOpen',
-    image: '/img/makeitopen.png',
-    infoLink: 'http://makeitopen.com/',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'React Native',
-    image: '/img/react-native.svg',
-    infoLink: 'https://facebook.github.io/react-native',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'Relay',
-    image: '/img/relay.svg',
-    infoLink: 'https://facebook.github.io/relay/',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'Bucklescript',
-    image: '/img/bucklescript.svg',
-    infoLink: 'https://bucklescript.github.io/',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: 'Docusaurus',
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.docusaurus.io',
-    fbOpenSource: true,
-    pinned: true,
-  },
-  {
-    caption: "Almin",
-    image: "/img/almin.png",
-    infoLink: "https://almin.js.org/",
-    fbOpenSource: false,
-    pinned: false,
-  },
-];
-
 const siteConfig = {
   title: 'Vuls',
   tagline: 'Agentless Vulnerability Scanner for Linux/FreeBSD',
@@ -117,6 +44,7 @@ const siteConfig = {
   headerIcon: 'img/docs/vuls_icon.png',
   footerIcon: 'img/docs/vuls_icon.png',
   favicon: 'img/docs/vuls_icon.png',
+  gaTrackingId: 'UA-112589928-1',
   /* colors for website */
   colors: {
     primaryColor: '#1b47b8',
